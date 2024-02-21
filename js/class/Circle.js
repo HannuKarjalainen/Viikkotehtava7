669a84f134d69d24bac7ede76bf1310162b1b421
HannuKarjalainen <143908228+HannuKarjalainen@users.noreply.github.com>
2024-02-21 22:35:18 +0200

    Update Circle.js

--- js/class/Circle.js
+++ js/class/Circle.js
@@ -12,6 +12,7 @@ export class Circle extends Shape {
         ctx.beginPath()
         ctx.arc(this._x,this._y,this.#radius, 0, 2 * Math.PI)
         ctx.lineWidth = this._lineWidth
+        ctx.strokeStyle = this._color;
         ctx.stroke()
     }
 }
