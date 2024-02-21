2c0e793fb79bf6df417267b4e825311f043ae0c8
HannuKarjalainen <143908228+HannuKarjalainen@users.noreply.github.com>
2024-02-21 22:34:28 +0200

    Update Shape.js

--- js/class/Shape.js
+++ js/class/Shape.js
@@ -2,14 +2,20 @@ export class Shape {
     _x
     _y
     _lineWidth
+    _color
 
-    constructor (x,y,_lineWidth = 1) {
+    constructor (x,y,lineWidth = 1, color = '#000000') {
         this._x = x;
         this._y = y;
-        this._lineWidth = this._lineWidth
+        this._lineWidth = lineWidth
+        this._color = color
     }
 
     set setLineWidth(lineWidth){
         this._lineWidth = lineWidth
+    }
+
+    set setColor(color){
+        this._color = color
     }
  }
